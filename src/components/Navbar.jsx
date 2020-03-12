d7974dda284bc4ca1b5891de02d7ad540a84e228
VladimirSolovyov <vovanchik3007@mail.ru>
2020-03-12 17:08:11 +0300

    css-module first part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="nav">
-      <div>
+      <div className='item'>
         <a href="%s"> Profile </a>
       </div>
-      <div>
+      <div className='item'>
         <a href="%s"> Message </a>
       </div>
-      <div>
+      <div className='item'>
         <a href="%s"> News </a>
       </div>
-      <div>
+      <div className='item'>
         <a href="%s"> Music </a>
       </div>
-      <div>
+      <div className='item'>
         <a href="%s"> Settings </a>
       </div>
     </nav>
